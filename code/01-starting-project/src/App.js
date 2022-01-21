3163d229c152157be19e2b7a68a306555add770c
paramintrt94 <36721661+paramintrt94@users.noreply.github.com>
2022-01-20 22:29:53 -0500

    Intro to React Router
    Introduction to React Router

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -1,7 +1,20 @@
+import { Route } from "react-router-dom";
+import Welcome from "./routes/Welcome";
+import Products from "./routes/Products";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+      <Route path="/lala">
+        <h2>Let's get started!</h2>
+      </Route>
     </div>
   );
 }
